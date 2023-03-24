7baa08cc1f56532e4653fbcdb276a4576521aa40
clara_grant <claragrant12@yahoo.com>
2023-03-24 16:47:00 +0000

    commit app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import Topics from './assets/topics'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Word cloud app', () => {
+  const topics = Topics.topics
+
+  test('renders Cloud word',() => {
+    const container = render(<App />);
+    expect(container).toMatchSnapshot()
+    expect(screen.getAllByRole('button').length).toEqual(topics.length)
+  })
+
+  test('renders Metadata', () => {
+    render(<App />);
+    expect(screen.getByTestId('metadata-header')).toBeInTheDocument()
+    expect(screen.getByTestId('metadata-header')).toHaveTextContent(topics[0].label)
+    expect(screen.getByTestId('positive-mention')).toHaveTextContent(topics[0].sentiment.positive)
+    expect(screen.getByTestId('neutral-mention')).toHaveTextContent(topics[0].sentiment.neutral)
+    expect(screen.getByTestId('negative-mention')).toHaveTextContent(topics[0].sentiment.negative)
+  })
+
+  test('updates Metadata on click', () => {
+    render(<App />);
+    const buttonClicked = screen.getAllByRole('button')[1]
+    fireEvent.click(buttonClicked)
+    expect(screen.getByTestId('metadata-header')).toHaveTextContent(topics[1].label)
+    expect(screen.getByTestId('positive-mention')).toHaveTextContent(topics[1].sentiment.positive)
+    expect(screen.getByTestId('neutral-mention')).toHaveTextContent(topics[1].sentiment.neutral)
+    expect(screen.getByTestId('negative-mention')).toHaveTextContent(0)
+  })
 });
